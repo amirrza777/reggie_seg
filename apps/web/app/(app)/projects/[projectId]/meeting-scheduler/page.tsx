9cacf081a3a9a20462f2e3b376d612ca8632d392
Matthew Kelsey <matthew.kelsey@kcl.ac.uk>
2026-03-02 19:00:52 +0000

    Add feature flags to meeting pages

--- apps/web/app/(app)/projects/[projectId]/meeting-scheduler/page.tsx
+++ apps/web/app/(app)/projects/[projectId]/meeting-scheduler/page.tsx
@@ -1,5 +1,6 @@
 import { MeetingSchedulerContent } from "@/features/meetings/components/MeetingSchedulerContent";
 import { ProjectNav } from "@/features/projects/components/ProjectNav";
+import { getFeatureFlagMap } from "@/shared/featureFlags";
 
 type PageProps = {
   params: Promise<{ projectId: string }>;
@@ -8,10 +9,11 @@ type PageProps = {
 export default async function MeetingSchedulerPage({ params }: PageProps) {
   const { projectId } = await params;
   const teamId = Number(projectId);
+  const flagMap = await getFeatureFlagMap();
 
   return (
     <div className="stack">
-      <ProjectNav projectId={projectId} />
+      <ProjectNav projectId={projectId} enabledFlags={flagMap} />
       <MeetingSchedulerContent teamId={teamId} projectId={teamId} />
     </div>
   );
